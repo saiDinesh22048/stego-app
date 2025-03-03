ef9a35aea90322ffe2efcd6e46ffeb7a0f6e0004
saiDinesh22048 <118376163+saiDinesh22048@users.noreply.github.com>
2025-03-04 00:46:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
 
-st.title('Stego-App')
+st.title('🕵️Stego-App')
 
-st.write('hide and seek of images')
+st.info('hide and seek of images')
