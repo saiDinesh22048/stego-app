5b07ebe72352c5c7aaacdd6e1b89851ef1b0a8f6
saiDinesh22048 <118376163+saiDinesh22048@users.noreply.github.com>
2025-03-05 02:48:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -322,9 +322,9 @@ if "logged_in" in st.session_state:
                 ])
                 with torch.no_grad():
                     
-                    stego_pil = tensor_to_pil(stego_image, mean, std)
+                    
                     stego_tensor = transform(image).unsqueeze(0).to(device)
-                    revealed_secret = reveal_net(stego_pil)
+                    revealed_secret = reveal_net(stego_tensor)
                     revealed_pil = tensor_to_pil(revealed_secret, mean, std)
 
                 extracted_secret =revealed_pil  
